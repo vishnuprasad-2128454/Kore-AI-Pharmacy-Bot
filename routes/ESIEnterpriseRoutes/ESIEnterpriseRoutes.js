67016c813babda9343b23f2d864ad19880a65840
Vignesh S <vignesh.s98@cognizant.com>
2024-12-13 10:14:54 +0530

    minor change

--- routes/ESIEnterpriseRoutes/ESIEnterpriseRoutes.js
+++ routes/ESIEnterpriseRoutes/ESIEnterpriseRoutes.js
@@ -17,7 +17,7 @@ router.use(function (req, res, next) {
     "Access-Control-Allow-Headers",
     "x-access-token, Origin, Content-Type, Accept"
   );
-  console.log("req",JSON.stringify(req))
+  console.log("req",req)
   next();
 });
 
