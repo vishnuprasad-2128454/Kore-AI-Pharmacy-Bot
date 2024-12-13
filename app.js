f87dda07cb63cd74a04abcbdd13579efbc8a0f26
Vignesh S <vignesh.s98@cognizant.com>
2024-12-13 12:44:11 +0530

    minor change

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ const constants = require('./constants/botConstants');
 sdk.checkNodeVersion();
 server.start();
 
-constants.botConversationResponse.verbiage_En_RespData = verbiageBuilder("ESI_PHA_BOT_RESP_BUILDER_EN_CA.xlsx");
-constants.botConversationResponse.verbiage_Fr_RespData = verbiageBuilder("ESI_PHA_BOT_RESP_BUILDER_FR_CA.xlsx");
+constants.botConversationResponse.verbiage_En_RespData = verbiageBuilder("./resources/ESI_PHA_BOT_RESP_BUILDER_EN_CA.xlsx");
+constants.botConversationResponse.verbiage_Fr_RespData = verbiageBuilder("./resources/ESI_PHA_BOT_RESP_BUILDER_FR_CA.xlsx");
 
 
 sdk.registerBot(require("./PharmacyBot.js"));
